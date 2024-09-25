63008d58587f2bd8992573f8e1468125f6b3ab81
Danish <muhammeddanishkv05@gmail.com>
2024-09-25 09:28:07 +0530

    feat: added logout logic in tutor sidebar

--- src/features/profile/student/components/ProfileSideBar.jsx
+++ src/features/profile/student/components/ProfileSideBar.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 import { User, Edit, Lock, Book, LogOut } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useDispatch } from "react-redux";
+import { Logout } from "@/redux/thunk/authThunks";
 
 const ProfileSideBar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate(); 
+
+  const handleLogout = async () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will be logged out from your account.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, log me out!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await dispatch(Logout()).unwrap();
+          navigate("/");
+
+          await Swal.fire({
+            title: "Logged out!",
+            text: "You have been successfully logged out.",
+            icon: "success",
+          });
+
+        } catch (error) {
+          console.log("logout error", error);
+          Swal.fire({
+            title: "Error!",
+            text: "There was an issue logging you out.",
+            icon: "error",
+          });
+        }
+      }
+    });
+  };
+
   return (
     <div className="flex h-screen bg-gray-100">
       <aside className="w-64 bg-white p-4">
@@ -36,8 +75,12 @@ const ProfileSideBar = () => {
             </Button>
           </Link>
 
-          <Link >
-            <Button variant="ghost" className="w-full justify-start">
+          <Link>
+            <Button
+              onClick={handleLogout}
+              variant="ghost"
+              className="w-full justify-start"
+            >
               <LogOut className="mr-2 h-4 w-4" />
               Logout
             </Button>
