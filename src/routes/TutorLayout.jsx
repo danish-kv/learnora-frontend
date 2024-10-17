167f3413c55b8bc0d7e67a68273c922b7c316d7c
Danish <muhammeddanishkv05@gmail.com>
2024-10-17 12:52:44 +0530

    removed sidebar and tutor header from application

--- src/routes/TutorLayout.jsx
+++ src/routes/TutorLayout.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NotFound from "../components/common/NotFound";
 import ProtectedRoute from "./protectedRoutes/ProtectedRoute";
 import AuthRouteProtection from "./protectedRoutes/AuthRouteProtection";
@@ -60,151 +60,213 @@ const TutorCommunity = lazy(() =>
 );
 
 const TutorLayout = () => {
+  const location = useLocation();
+  const isApplicationRoute = location.pathname.includes("/application");
+
   return (
     <div className="flex h-screen">
-    <TutorSidebar />
-    <div className="flex-1 flex flex-col overflow-hidden">
-      <TutorHeader />
-      <main className="flex-1 overflow-x-hidden overflow-y-auto">
+      {!isApplicationRoute && <TutorSidebar />}
+      <div
+        className={`flex-1 flex flex-col overflow-hidden ${
+          isApplicationRoute ? "w-full" : ""
+        }`}
+      >
+        {!isApplicationRoute && <TutorHeader />}
+        <main className="flex-1 overflow-x-hidden overflow-y-auto">
+          <Suspense fallback={<LoadingDotStream />}>
+            <Routes>
+              <Route
+                path="application"
+                element={
+                  <AuthRouteProtection
+                    element={<TutorApplication />}
+                    redirectTo={"/tutor"}
+                  />
+                }
+              />
 
-    <Suspense fallback={<LoadingDotStream />}>
-      <Routes>
-        <Route
-          path="application"
-          element={
-            <AuthRouteProtection
-              element={<TutorApplication />}
-              redirectTo={"/tutor"}
-            />
-          }
-        />
+              <Route
+                path="application/done"
+                element={<TutorApplicationDone />}
+              />
+              <Route
+                path=""
+                element={
+                  <ProtectedRoute element={<TutorDashboard />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="courses"
+                element={
+                  <ProtectedRoute element={<TutorCourse />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="profile"
+                element={
+                  <ProtectedRoute element={<TutorProfile />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="new-course"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCreateCourse />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="course/:slug"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCourseDetails />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="new-module/:id"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCreateModule />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="course/edit/:slug"
+                element={
+                  <ProtectedRoute
+                    element={<TutorEditCourse />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="module/edit/:id"
+                element={
+                  <ProtectedRoute
+                    element={<TutorEditModule />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/categories"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCategories />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/contest"
+                element={
+                  <ProtectedRoute element={<TutorContest />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="/contest/create"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCreateContest />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/contest/questions/create/:id"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCreateQuestion />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/contest/:id"
+                element={
+                  <ProtectedRoute
+                    element={<TutorContestDetails />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/contest/:id/edit"
+                element={
+                  <ProtectedRoute
+                    element={<TutorEditContest />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/contest/:id/question/:id/edit"
+                element={
+                  <ProtectedRoute
+                    element={<TutorEditQuestion />}
+                    role={"tutor"}
+                  />
+                }
+              />
 
-        <Route path="application/done" element={<TutorApplicationDone />} />
-        <Route
-          path=""
-          element={
-            <ProtectedRoute element={<TutorDashboard />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="courses"
-          element={<ProtectedRoute element={<TutorCourse />} role={"tutor"} />}
-        />
-        <Route
-          path="profile"
-          element={<ProtectedRoute element={<TutorProfile />} role={"tutor"} />}
-        />
-        <Route
-          path="new-course"
-          element={
-            <ProtectedRoute element={<TutorCreateCourse />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="course/:slug"
-          element={
-            <ProtectedRoute element={<TutorCourseDetails />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="new-module/:id"
-          element={
-            <ProtectedRoute element={<TutorCreateModule />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="course/edit/:slug"
-          element={
-            <ProtectedRoute element={<TutorEditCourse />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="module/edit/:id"
-          element={
-            <ProtectedRoute element={<TutorEditModule />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/categories"
-          element={
-            <ProtectedRoute element={<TutorCategories />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/contest"
-          element={<ProtectedRoute element={<TutorContest />} role={"tutor"} />}
-        />
-        <Route
-          path="/contest/create"
-          element={
-            <ProtectedRoute element={<TutorCreateContest />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/contest/questions/create/:id"
-          element={
-            <ProtectedRoute element={<TutorCreateQuestion />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/contest/:id"
-          element={
-            <ProtectedRoute element={<TutorContestDetails />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/contest/:id/edit"
-          element={
-            <ProtectedRoute element={<TutorEditContest />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/contest/:id/question/:id/edit"
-          element={
-            <ProtectedRoute element={<TutorEditQuestion />} role={"tutor"} />
-          }
-        />
+              <Route
+                path="/community"
+                element={
+                  <ProtectedRoute element={<TutorCommunity />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="/community/create"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCreateCommunity />}
+                    role={"tutor"}
+                  />
+                }
+              />
 
-        <Route
-          path="/community"
-          element={
-            <ProtectedRoute element={<TutorCommunity />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/community/create"
-          element={
-            <ProtectedRoute element={<TutorCreateCommunity />} role={"tutor"} />
-          }
-        />
+              <Route
+                path="/community/:slug"
+                element={
+                  <ProtectedRoute
+                    element={<TutorCommunityChat />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute element={<TutorProfile />} role={"tutor"} />
+                }
+              />
+              <Route
+                path="/profile/:id"
+                element={
+                  <ProtectedRoute
+                    element={<TutorEditProfile />}
+                    role={"tutor"}
+                  />
+                }
+              />
+              <Route
+                path="/sales-report"
+                element={
+                  <ProtectedRoute
+                    element={<TutorSalesReport />}
+                    role={"tutor"}
+                  />
+                }
+              />
 
-        <Route
-          path="/community/:slug"
-          element={
-            <ProtectedRoute element={<TutorCommunityChat />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/profile"
-          element={<ProtectedRoute element={<TutorProfile />} role={"tutor"} />}
-        />
-        <Route
-          path="/profile/:id"
-          element={
-            <ProtectedRoute element={<TutorEditProfile />} role={"tutor"} />
-          }
-        />
-        <Route
-          path="/sales-report"
-          element={
-            <ProtectedRoute element={<TutorSalesReport />} role={"tutor"} />
-          }
-        />
-
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
-    </main>
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </main>
       </div>
     </div>
   );
